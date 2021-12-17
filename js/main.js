b30c3352d2338b4f962382572a65c8eef4ae4f95
e-doschechnikova <evadrobakova@gmail.com>
2021-12-17 21:06:42 +0300

    add simple validation for form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
 var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
 var closePopup = document.querySelector(".modal-close");
+var form = popup.querySelector("form");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -12,4 +14,11 @@ link.addEventListener("click", function (evt) {
 closePopup.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести логин и пароль");
+  }
 });
